2cb68a623889f29390dfc9f699219c63abf746e6
Omen Wild <omen@mandarb.com>
2018-09-17 21:32:38 -0700

    Still tweaking the setup.

--- z_wave_grapher/run.sh
+++ z_wave_grapher/run.sh
@@ -3,5 +3,6 @@ set -e
 
 cp /z-wave-graph.html /config/www
 cp /shell_commands_z-wave-graph.yaml /config/shell_commands/z-wave-graph.yaml
+cp /automations_z-wave-graph.yaml /config/automations/z-wave-graph.yaml
 
 python3 z-wave-graph.py
